7b55510f17230b54c643c26ec37145c5444328a5
Manthan-Pelne <manthanpelne1997@gmail.com>
2026-02-13 22:19:36 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,7 @@ const connection = require("./config/db")
 //middlewares
 app.use(express.json())
 app.use(cors({
-  origin: 'https://stately-cassata-87348e.netlify.app', // Your frontend URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: '*'
 }));
 
 
