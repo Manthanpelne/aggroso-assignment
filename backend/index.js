2a1a4b95e3b74997f06fd13a8770d2f827babdbd
Manthan-Pelne <manthanpelne1997@gmail.com>
2026-02-13 22:34:53 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -7,19 +7,8 @@ const connection = require("./config/db")
 
 //middlewares
 
-const allowedOrigins = ['http://localhost:5173/','https://stately-cassata-87348e.netlify.app/'];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS policy violation'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+  origin: "http://localhost:5173/"
 }));
 
 
