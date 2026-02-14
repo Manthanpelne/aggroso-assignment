363ee0d6ad2851be9fc6383c85e443f375fe341a
Manthan-Pelne <manthanpelne1997@gmail.com>
2026-02-14 10:20:11 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,18 @@ const connection = require("./config/db")
 //middlewares
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  // Handle Preflight
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.use(express.json())
 
 
